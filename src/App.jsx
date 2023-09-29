3592ea90588a1ce8afae2400c1607ae42efd8075
Jagannath Nayak <jagannathnayak2105@gmail.com>
2023-09-29 12:23:38 +0530

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import {Logo} from "../public/images/apple-touch-icon.png"
+
+function App() {
+  return (
+    <div className="">
+      <div className=''>
+        <img src={Logo} alt="Logo" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
